12a5e107c078f047c58a05b4d76dd9e362b1ab57
Dave Stanke <davidstanke@google.com>
2019-12-22 19:06:30 -0500

    add sam page

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ app.get('/', (req,res) => {
   res.send('Hi, this is the Stanke/George family site.')
 })
 
+app.get('/sam', (req,res) => {
+  res.send('<h1>I am Sam and I am 8 years old.</h1><p>Do you like my typing?</p>')
+})
+
 app.listen(port, () => {
   console.log('Server is listening on port ' + port)
 })
